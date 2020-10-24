44d930a7c96e2c576be07840b7549b255d95227d
kupriyanov-19 <58529175+kupriyanov-19@users.noreply.github.com>
2020-10-24 17:23:11 +0300

    Update Revier1_Interface.cpp

--- Revier1_Interface.cpp
+++ Revier1_Interface.cpp
@@ -11,20 +11,20 @@ struct Edge
 };
 
 // Function for multiplication two matrices
-squareMatrix multiplication(const squareMatrix& matrixFirst, const squareMatrix& matrixSecond);
+squareMatrix multiplyMatrices(const squareMatrix& matrixFirst, const squareMatrix& matrixSecond);
 
 // Function for creating a identity matrix
-squareMatrix identityMatrix(const int matrixSize);
+squareMatrix createIdentityMatrix(const int matrixSize);
 
 // Function for binary exponentiation
-squareMatrix binaryPower(const squareMatrix& matrix, const int power);
+squareMatrix getMatrixPower(const squareMatrix& matrix, const int power);
 
 // Function for read one number
 int readNumber(std::istream&);
 
 std::vector<Edge> readEdges(std::istream&, const int numberOfEdges);
 
-squareMatrix initializationMatrix(const std::vector<Edge>& ,const int matrixSize);
+squareMatrix createAdjacencyMatrix(const std::vector<Edge>&, const int matrixSize);
 
 // Function to calculate the sum in the first line of matrix
 int countSumAtFirstLine(const squareMatrix& matrix);
@@ -34,12 +34,12 @@ void printNumber(std::ostream&, const int number);
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  const int numberOfRooms = readNumber(std::cin),
-    numberOfEdges = readNumber(std::cin),
-    power = readNumber(std::cin);
+  const int numberOfRooms = readNumber(std::cin);
+  const int numberOfEdges = readNumber(std::cin);
+  const int power = readNumber(std::cin);
   const std::vector<Edge> allEdges = readEdges(std::cin, numberOfEdges);
-  const squareMatrix matrix = initializationMatrix(allEdges, numberOfRooms);
-  const squareMatrix transformedMatrix = binaryPower(matrix, power);
+  const squareMatrix matrix = createAdjacencyMatrix(allEdges, numberOfRooms);
+  const squareMatrix transformedMatrix = getMatrixPower(matrix, power);
   const int sumAtFirstLine = countSumAtFirstLine(transformedMatrix);
   printNumber(std::cout, sumAtFirstLine);
   return 0;
