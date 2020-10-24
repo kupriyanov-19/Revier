688ca96033256fac74b59ca810cdb183bdc65c69
kupriyanov-19 <58529175+kupriyanov-19@users.noreply.github.com>
2020-10-24 11:44:01 +0300

    Update Revier1_Interface.cpp

--- Revier1_Interface.cpp
+++ Revier1_Interface.cpp
@@ -2,7 +2,13 @@
 #include<vector>
 typedef std::vector<std::vector<int64_t>> squareMatrix;
 
-const int64_t modulo = 1000000000 + 7;
+const int64_t MODULO = 1000000000 + 7;
+
+// Structure for representation edges
+struct Edge
+{
+  int edgeStart, edgeEnd;
+};
 
 // Function for multiplication two matrices
 squareMatrix multiplication(const squareMatrix& matrixFirst, const squareMatrix& matrixSecond);
@@ -16,7 +22,9 @@ squareMatrix binaryPower(const squareMatrix& matrix, const int power);
 // Function for read one number
 int readNumber(std::istream&);
 
-squareMatrix readMatrix(std::istream&, const int matrixSize, const int numberOfEdges);
+std::vector<Edge> readEdges(std::istream&, const int numberOfEdges);
+
+squareMatrix initializationMatrix(const std::vector<Edge>& ,const int matrixSize);
 
 // Function to calculate the sum in the first line of matrix
 int countSumAtFirstLine(const squareMatrix& matrix);
@@ -26,9 +34,13 @@ void printNumber(std::ostream&, const int number);
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  const int numberOfRooms=readNumber(std::cin), numberOfEdges=readNumber(std::cin), power=readNumber(std::cin);
-  const squareMatrix matrix= readMatrix(std::cin, numberOfRooms, numberOfEdges);
-  const int sumAtFirstLine=countSumAtFirstLine(binaryPower(matrix, power));
+  const int numberOfRooms = readNumber(std::cin),
+    numberOfEdges = readNumber(std::cin),
+    power = readNumber(std::cin);
+  const std::vector<Edge> allEdges = readEdges(std::cin, numberOfEdges);
+  const squareMatrix matrix = initializationMatrix(allEdges, numberOfRooms);
+  const squareMatrix transformedMatrix = binaryPower(matrix, power);
+  const int sumAtFirstLine = countSumAtFirstLine(transformedMatrix);
   printNumber(std::cout, sumAtFirstLine);
   return 0;
 }
